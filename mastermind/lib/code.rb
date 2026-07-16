e1e027f18128b9d8f571b4adca20818e41d0ac38
Ghr4b <graba.hadi@gmail.com>
2026-07-16 19:44:14 +0100

    masstermind

--- mastermind/lib/code.rb
+++ mastermind/lib/code.rb
@@ -1,22 +1,47 @@
 class Code
-  COLORS = %w[red orange yellow green blue purple]
-  attr_reader :secret
-  def initialize(secret)
-    raise ArgumentError, "Invalid code: #{secret}" unless validate_code(secret)
-    @secret = secret
+  COLORS = %w[red blue green yellow purple orange]
+
+  attr_reader :secret_code
+
+  def initialize(secret_code)
+    raise ArgumentError, "Invalid code: #{secret_code}" unless validate_code(secret_code)
+    @secret_code = secret_code
   end
 
   def check_guess(guess)
     raise ArgumentError, "Invalid guess: #{guess}" unless validate_code(guess)
-    exact_matches = secret.zip(guess).select { |s, g| s == g } .map(&:first)
-    reds = exact_matches.count
-    whites = (guess - exact_matches).count { |c| (secret - exact_matches).include?(c) }
+
+    exact_indices = []
+    secret_code.each_with_index do |s, i|
+      exact_indices << i if s == guess[i]
+    end
+
+    reds = exact_indices.count
+
+    leftover_secret = secret_code.each_with_index.reject { |_, i| exact_indices.include?(i) }.map(&:first)
+    leftover_guess  = guess.each_with_index.reject  { |_, i| exact_indices.include?(i) }.map(&:first)
+
+    whites = 0
+    leftover_guess.each do |color|
+      whiteindex = leftover_secret.index(color)
+      if whiteindex
+        whites += 1
+        leftover_secret.delete_at(whiteindex)
+      end
+    end
+
     [reds, whites]
   end
-end
-def validate_code(code)
-  if code.length != 4 or code.chars.any? { |c| !Code::COLORS.include?(c) }
-    return false
+
+  private
+
+  def validate_code(code)
+    code_array = code.is_a?(String) ? code.chars : code
+
+    if code_array.length != 4 or code_array.any? { |c| !COLORS.include?(c) }
+      return false
+    end
+
+    true
   end
-  return true
 end

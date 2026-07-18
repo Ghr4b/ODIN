ae32269f0a02a23fbc6e4be74e8fcc3af718a8d9
Ghr4b <graba.hadi@gmail.com>
2026-07-18 19:33:23 +0100

    done with the testing course

---
+++ ruby_testing/lib/14_find_number.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# class for computer to find random number
+class FindNumber
+  attr_reader :min, :max, :answer, :guess
+
+  def initialize(min, max, answer = RandomNumber.new(min, max), guess = nil)
+    @min = min
+    @max = max
+    # The RandomNumber class will have an instance variable '@value'
+    @answer = answer.value
+    @guess = guess
+  end
+  def make_guess
+    @guess = (@min + @max) / 2
+    @guess
+  end
+  def game_over?
+    @guess == @answer
+  end
+  def update_range
+    if @guess < @answer
+      @min = @guess + 1
+    else
+      @max = @guess - 1
+    end
+  end
+end
